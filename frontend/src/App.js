e5d8c1d695eef3c9f51f313a5184a231596c2ac1
sharath778 <skreamlme778@gmail.com>
2025-01-04 14:06:02 +0530

    added the drop down field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,35 @@
-// import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Home from './components/Home';
 import Login from './components/Login';
 import Header from './components/Header';
-import { ToastContainer } from 'react-toastify';
-
-import Staff from './pages/Staff.jsx';
-import StaffProducts from './components/StaffProducts.jsx';
-import Notifications from './components/Notification.jsx';
-import Transactions from './components/Transactions.jsx';
-import Suppliers from './components/Suppliers.jsx';
-
-
+import Staff from './pages/Staff';
+import StaffProducts from './components/StaffProducts';
+import Notifications from './components/Notification';
+import Transactions from './components/Transactions';
+import Suppliers from './components/Suppliers';
 import Register from './components/Register';
 import Products from './components/Products';
 
 const App = () => {
-
-
-
-
   return (
-    <div>
-      <ToastContainer/>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/products' element={<Products />} />
+    <Router>
+      <ToastContainer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/staff" element={<Staff />} />
+        <Route path="/staff-products" element={<StaffProducts />} />
+        <Route path="/suppliers" element={<Suppliers />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+};
 
-
-          {/* <Route path="*" element={<Error />} /> */}
-          <Route path='/staff' element={<Staff />} />
-          <Route path='/staff-products' element={<StaffProducts />} />
-          <Route path='/suppliers' element={<Suppliers />} />
-          <Route path='/transactions' element={<Transactions />} />
-          <Route path='/notifications' element={<Notifications />} />
-
-        </Routes>
-      </Router>
-    </div>
-  )
-}
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
